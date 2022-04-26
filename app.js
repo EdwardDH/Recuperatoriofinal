464ec16db0d0bb29301fe6bdbc36f9b601b08cf9
edwardcf <edward.casanova@hotmail.com>
2022-04-26 18:10:12 -0300

    ultimos cambios

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const turnoController = require('../controller/turnoController.js')
+const turnoController = require('./controller/turnoController.js')
 app.use(express.static("public"));
 
 
 
 app.get('/admin/turnos', turnoController.adminTurnos)
 
-app.get('/listado_turnos', turnoController.listado_turnos)
+app.get('/listadoTurnos', turnoController.listadoTurnos)
 
 app.get('/login', (req, res) => {
     return res.sendFile(path.join(__dirname,'/views/login.html'));
 })
 
-app.get('/vista_turnos', turnoController.vista_turnos)
+app.get('/vistaTurnos', turnoController.vistaTurnos)
 
 app.listen(3000, () => console.log('corriendo'));
--- controller/turnoController.js
+++ controller/turnoController.js
@@ -1,13 +1,14 @@
 const path = require('path')
+
 const turnoController = {
-    listado_turnos: (req, res) => {
-        return res.sendFile(path.join(__dirname,'/views/listado_turnos.html'));
+    listadoTurnos: (req, res) => {
+        return res.sendFile(path.join(__dirname,'../views/listadoTurnos.html'));
     },
     adminTurnos: (req, res) => {
-        return res.sendFile(path.join(__dirname,'/views/admin/turnos.html'));
+        return res.sendFile(path.join(__dirname,'../views/admin/turnos.html'));
     },
-    vista_turnos: (req, res) => {
-        return res.sendFile(path.join(__dirname,'/views/vista_turnos.html'));
+    vistaTurnos: (req, res) => {
+        return res.sendFile(path.join(__dirname,'../views/vistaTurnos.html'));
     }
 
 };
